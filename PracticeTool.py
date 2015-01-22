db11a13446750bbebf2bc31e8efdcf90dea72a41
xtraman297 <xtraman297@gmail.com>
2015-01-22 08:27:17 +0200

    First upload

---
+++ PracticeTool.py
@@ -0,0 +1,233 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt4 tutorial 
+
+This example shows text which 
+is entered in a QtGui.QLineEdit
+in a QtGui.QLabel widget.
+ 
+author: Jan Bodnar
+website: zetcode.com 
+last edited: August 2011
+"""
+STRINGS_ON_GUITAR = 6
+import sys
+from random import randint, randrange, choice
+from winsound import Beep         # for sound
+import RandomScaleLib
+import sfml as sf
+from time import sleep
+import threading
+from PyQt4 import QtGui, QtCore
+
+note_to_number = {'C': 0, 'C#/Db': 1, 'D': 2, 'D#/Eb': 3, 'E': 4, 'F': 5, 'F#/Gb': 6, 'G': 7, 'G#/Ab': 8, 'A': 9, 'A#/Bb': 10, 'B': 11}
+all_notes = ['C', 'C#/DB', 'D', 'D#/EB', 'E', 'F', 'F#/GB', 'G', 'G#/AB', 'A', 'A#/BB', 'B']
+notes_tuning_arr = ["e", "B", "G", "D", "A", "E"]
+
+key_freq = {'A': 440, 'B': 493, 'C': 523, 'D': 587, 'E': 659, 'F': 698, 'G': 783,
+            'G#/Ab': 830, 'A#/Bb': 466, 'F#/Gb': 739, 'C#/Db': 554, 'D#/Eb': 622}
+
+class Example(QtGui.QWidget):
+    
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        list = (1, 1, 1, 2, 1), (1, 2, 2), (1, 2, 3), (1, 2, 4), (1, 2, 5), (1, 2, 6)
+
+        self.freq = key_freq['A']
+
+        self.start_metronome = QtGui.QPushButton('Start', self)
+        self.start_metronome.move(10, 10)
+        self.start_metronome.setMinimumSize(100, 100)
+        self.start_metronome.clicked.connect(self.stopStartMetronome)
+        self.start_metronome.started = False
+        self.thread1 = threading.Thread(target=self.print_loop)
+
+        self.change_scale = QtGui.QPushButton('Change Scale', self)
+        self.change_scale.move(120, 10)
+        self.change_scale.setMinimumSize(100, 100)
+        self.change_scale.clicked.connect(self.stopStartMetronome)
+
+        self.num_of_strings = QtGui.QPushButton('Set number of strings', self)
+        self.num_of_strings.move(230, 10)
+        self.num_of_strings.setMinimumSize(100, 100)
+        self.num_of_strings.clicked.connect(self.stopStartMetronome)
+
+        self.change_tuning = QtGui.QPushButton('Change Tuning', self)
+        self.change_tuning.move(350, 10)
+        self.change_tuning.setMinimumSize(100, 100)
+        self.change_tuning.clicked.connect(self.stopStartMetronome)
+
+        self.change_tabs_mode = QtGui.QPushButton('Change Tabs Mode', self)
+        self.change_tabs_mode.move(460, 10)
+        self.change_tabs_mode.setMinimumSize(100, 100)
+        self.change_tabs_mode.clicked.connect(self.stopStartMetronome)
+
+        self.hide_scale = QtGui.QPushButton('Unhide', self)
+        self.hide_scale.move(10, 175)
+        self.hide_scale.setMinimumSize(100, 10)
+        self.hide_scale.setToolTip('This will hide/unhide the scale')
+        self.hide_scale.resize(self.hide_scale.sizeHint())
+        self.hide_scale.clicked.connect(self.clickButton)
+        self.hide_scale.moshe = True
+
+        self.txt = QtGui.QTextEdit(self)
+        self.txt.move(10, 200)
+        self.txt.setEnabled(False)
+        self.txt.setMinimumWidth(550)
+        self.txt.setMaximumHeight(95)
+        self.txt.geometry()
+        self.txt.insertPlainText(print_tab_full(get_scale_return_array('D#/EB', 1, 1)))
+        self.txt.adjustSize()
+
+        self.setGeometry(750, 700, 570, 310)
+        self.setWindowTitle('Guitar Trainer')
+        self.show()
+
+        self.clickButton()
+        get_scale_return_array('E', 1, 1)
+
+    def onChanged(self, text):
+        self.txt.setText(text)
+
+    def clickButton(self):
+        if self.hide_scale.moshe:
+            self.txt.setStyleSheet('color:#FFF8DC;Background:#FFF8DC;')
+            self.hide_scale.setText('Unhide')
+        else:
+            self.txt.setStyleSheet('font-family: "Courier New", Courier, monospace;Background:#FFF8DC;')
+            self.hide_scale.setText('Hide')
+        self.hide_scale.moshe = not self.hide_scale.moshe
+
+    def stopStartMetronome(self):
+
+        if self.start_metronome.started:
+            self.start_metronome.setStyleSheet("background-color: light gray")
+            self.start_metronome.setText('start')
+            self.start_metronome.started = False
+        else:
+            self.start_metronome.setStyleSheet("background-color: red")
+            self.start_metronome.setText('stop')
+            self.start_metronome.started = True
+            self.thread1 = threading.Thread(target=self.print_loop)
+            self.thread1.start()
+            #print 1
+
+    def print_loop(self):
+        local_count = 0
+        weight = randint(4, 4)
+
+        tempo = (float(randint(25, 100)) * 0.01)
+        while self.start_metronome.started:
+            #print (str(local_count + 1), end='')
+            local_count = (local_count + 1) % weight
+            if local_count == 1:
+                Beep(self.freq * 2, 250)     # frequency, duration
+            else:
+                Beep(self.freq, 250)     # frequency, duration
+            sleep(tempo - 0.25)           # in seconds (0.25 is 250ms)
+        return
+
+
+
+def print_tab_3_per_string(list):
+    note_tab_arr = ["e", "B", "G", "D", "A", "E"]
+    #some_string = ''
+    some_array = []
+    for i in range(0, 6):
+        some_array.append(note_tab_arr[i])
+        some_array.append("|---")
+
+        for aft in range(i, STRINGS_ON_GUITAR-1):
+                for z in list[aft]:
+                    some_array.append("----")
+        for x in list[i]:
+            if x == -1:
+                some_array.append("---")
+            else:
+                some_array.append(str(x))
+            if x < 10:
+                some_array.append("---")
+            else:
+                some_array.append("--")
+        for bef in range(0, i):
+                for z in list[bef]:
+                    some_array.append("----")
+        some_array.append("\n")
+    some_string = "".join(some_array)
+
+    return some_string[:-1] # [:-1] -> Removing last \n
+
+def print_tab_full(list):
+    #note_tab_arr = ["e", "B", "G", "D", "A", "E"]
+    #some_string = ''
+    some_array = []
+    for i in range(0, 6):
+        some_array.append(notes_tuning_arr[i])
+        some_array.append("|---")
+
+        #for aft in range(i, STRINGS_ON_GUITAR-1):
+        #        for z in list[aft]:
+        #            some_array.append("----")
+        for x in list[i]:
+            if x == -1:
+                some_array.append("-")
+            else:
+                some_array.append(str(x))
+            if x < 10:
+                some_array.append("---")
+            else:
+                some_array.append("--")
+        #for bef in range(0, i):
+        #        for z in list[bef]:
+        #            some_array.append("----")
+        some_array.append("\n")
+    some_string = "".join(some_array)
+
+    return some_string[:-1] # [:-1] -> Removing last \n
+
+def get_scale_return_array(scale_note, tunning_intervals, starting_note):
+    major_array = [2, 2, 1, 2, 2, 2, 1]
+    scale_pointer = 0
+
+    twelve_note_list = []
+    scale_list = []
+    scale_temp = []
+    start_tmp = all_notes.index(scale_note)
+    major_num = 0
+
+    for num in xrange(all_notes.index(scale_note), all_notes.index(scale_note)+7):
+        #print start_tmp % len(all_notes)
+        scale_temp.append(all_notes[start_tmp % len(all_notes)])
+        start_tmp += major_array[major_num]
+        major_num += 1
+    #print scale_temp
+
+    for num in xrange(len(notes_tuning_arr)):
+        note_index = all_notes.index(notes_tuning_arr[num].upper())
+        for tab in xrange(13):
+            if all_notes[note_index % len(all_notes)] in scale_temp:
+                twelve_note_list.append(tab)
+            else:
+                twelve_note_list.append(-1)
+            note_index += 1
+        scale_list.append(twelve_note_list[:])
+        twelve_note_list = []
+    return scale_list
+
+
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
---
+++ RandomScaleLib.py
@@ -0,0 +1,159 @@
+# By Eran Brodet and Moshe Simantov
+# 2014
+from __future__ import print_function
+
+__author__ = 'moshest'
+from os import system
+from msvcrt import getch
+from random import randint, randrange, choice
+from winsound import Beep         # for sound
+from time import sleep            # for sleep
+import thread
+
+note_to_number = {'C': 0, 'C#/Db': 1, 'D': 2, 'D#/Eb': 3, 'E': 4, 'F': 5, 'F#/Gb': 6, 'G': 7, 'G#/Ab': 8, 'A': 9, 'A#/Bb': 10, 'B': 11}
+
+keylist = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'G#/Ab', 'A#/Bb', 'F#/Gb', 'C#/Db', 'D#/Eb'
+keylist_to_E_string = {'A': 5, 'B': 7, 'C': 8, 'D': 10, 'E': 0, 'F': 1, 'G': 3, 'G#/Ab': 4, 'A#/Bb': 6, 'F#/Gb': 2, 'C#/Db': 9, 'D#/Eb': 11}
+key_freq = {'A': 440, 'B': 493, 'C': 523, 'D': 587, 'E': 659, 'F': 698, 'G': 783,
+            'G#/Ab': 830, 'A#/Bb': 466, 'F#/Gb': 739, 'C#/Db': 554, 'D#/Eb': 622}
+modeslist = 'Inonian', 'Dorian', 'Phrygian', 'Lydian', 'Mixolydian', 'Aeolian', 'Locrian'
+modeslist_to_degree = {'Inonian': 1, 'Dorian': 2, 'Phrygian': 3, 'Lydian': 4, 'Mixolydian': 5, 'Aeolian': 6, 'Locrian': 7}
+maj_min = 'Major', 'Minor'
+
+STRINGS_ON_GUITAR = 6
+
+def print_tab(list):
+    note_tab_arr = ["e", "B", "G", "D", "A", "E"]
+    for i in range(0, 6):
+        print (note_tab_arr[i], end='')
+        print ("|---", end='')
+
+        for aft in range(i, STRINGS_ON_GUITAR-1):
+                for z in list[aft]:
+                    print ("----", end='')
+        for x in list[i]:
+            if x == -1:
+                print ("---", end='')
+            else:
+                print (str(x), end='')
+            if x < 10:
+                print ("---", end='')
+            else:
+                print ("--", end='')
+        for bef in range(0, i):
+                for z in list[bef]:
+                    print ("----", end='')
+        print ('')
+
+def list_upside_down(list):
+    #tmp_list = [None] * len(list)
+    print (list[0])
+    print (list[1])
+    tmp_list = list
+    for i in list:
+        tmp_list[len(list) - i] = list[i]
+    return tmp_list
+
+def get_scale_return_array(scale_name, tunning_intervals, starting_note):
+    major_array = [2, 2, 1, 2, 2, 2, 1]
+    #scale_pointer = 0 is default for major scale
+    scale_pointer = 0
+    three_note_list = []
+    scale_list = []
+    #here will check for scale_name to change value of scale_pointer
+    #scale_pointer = modeslist_to_degree[scale_name]
+    for x in range(0, STRINGS_ON_GUITAR):
+        for y in range(0, 3):
+            three_note_list.append(starting_note)
+            starting_note += major_array[scale_pointer]
+            scale_pointer += 1
+            if scale_pointer == 7:
+                scale_pointer = 0
+        starting_note -= tunning_intervals[x]
+        scale_list.append(three_note_list[:])  # clone of line
+        three_note_list = []
+    return scale_list
+
+
+def input_thread(L):
+    """
+        Runs in UI thread
+    """
+    #f = getch()
+    #print f
+    #raw_input()
+    L.append('')
+    while True:
+        L[0] = getch()
+        #L.append(getch())
+
+
+def print_practice(tempo, key, weight, modos):
+    practice_type = randint(1, 2)
+
+    if practice_type == 1:
+        print (key + ' ' + modos, 'in tempo:', int((1 / tempo) * 60), 'in weight of', str(weight) + "'s")
+        #return modos
+    elif practice_type == 2:
+        print (modos + ' of ' + key + ' ' + choice(maj_min), 'in tempo:', int((1 / tempo) * 60), 'in weight of', str(weight) + "'s")
+        #return modos
+
+
+def metronome_simple(tempo, key, weight, modos):
+    L = []
+    thread.start_new_thread(input_thread, (L,))
+    local_count = 0
+    freq = key_freq[key]
+    print_scale_flag = 0
+    system('cls')
+    tuning_space = [5, 5, 5, 4, 5, 5]
+
+    print_practice(tempo, key, weight, modos)
+
+    tab_list = get_scale_return_array(modos, tuning_space, keylist_to_E_string[key])
+    tab_list.reverse()
+    print_tab(tab_list)
+    while True:
+        #print (str(local_count + 1), end='')
+        local_count = (local_count + 1) % weight
+        if local_count == 1:
+            Beep(freq * 2, 250)     # frequency, duration
+        else:
+            Beep(freq, 250)     # frequency, duration
+        sleep(tempo - 0.25)           # in seconds (0.25 is 250ms)
+        if print_scale_flag == 1:
+            print('SCALE')
+            tab_list = get_scale_return_array(mod_from_print, tuning_space, 3)
+            print_tab(tab_list)
+
+        print ('\r', end='')
+
+        #print L
+        if L[0] != '':
+            if L[0] == 'q':
+                L[0] = ''
+                exit()
+            elif (L[0] == 'h' or L[0] == 'H') and print_scale_flag == 0:
+                print_scale_flag = 1
+                L[0] = ''
+            elif (L[0] == 'h' or L[0] == 'H') and print_scale_flag == 1:
+                L[0] = ''
+                print_scale_flag = 0
+            else:
+                L[0] = ''
+                system('cls')
+                #print '\r\r',
+                break
+
+
+def main():
+    while True:
+        keychose = choice(keylist)
+        tempo = (float(randint(25, 100)) * 0.01)
+        weight = randint(4, 4)
+        modos = choice(modeslist)
+        metronome_simple(tempo, keychose, weight, modos)
+
+
+if __name__ == "__main__":
+    main()
